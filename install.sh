a62b87bd55b0d23e3dbef5fcf5cb6d5ef62c0eba
TGSP1997 <Gymisandro@web.de>
2023-04-03 11:35:19 +0200

    Added logic to install routine

--- install.sh
+++ install.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 set -e
+#Set Working Directory
 WORKING_DIR=$(pwd)
 
-git clone https://github.com/OpenTrafficCam/OTVision.git "$WORKING_DIR"/OpenTrafficCam/OTVision
-git clone https://github.com/OpenTrafficCam/OTAnalytics.git "$WORKING_DIR"/OpenTrafficCam/OTAnalytics
+#Install Venv
+if [ ! -d "$WORKING_DIR"/.venv ]; then
+  python -m venv .venv
+fi
 
+#Repros
+if [ ! -d "$WORKING_DIR"/ExtRepros/OTVision ]; then
+  git clone https://github.com/OpenTrafficCam/OTVision.git "$WORKING_DIR"/ExtRepros/OTVision
+fi
+if [ ! -d "$WORKING_DIR"/ExtRepros/OTAnalytics ]; then
+  git clone https://github.com/OpenTrafficCam/OTAnalytics.git "$WORKING_DIR"/ExtRepros/OTAnalytics
+fi
+
+#Requierements of ExtRepros
+source "$WORKING_DIR"/.venv/Scripts/activate
+python -m pip install --upgrade -r "$WORKING_DIR"/ExtRepros/OTVision/requirements.txt
+python -m pip install --upgrade -r "$WORKING_DIR"/ExtRepros/OTAnalytics/requirements.txt
+deactivate
