4eee698709023e195670a0582e5c2941c42d295b
TGSP1997 <Gymisandro@web.de>
2023-05-18 06:53:54 +0200

    restructure part 2

--- install.sh
+++ install.sh
@@ -11,5 +11,6 @@ fi
 
 #Requierements of ExtRepros
 source "$WORKING_DIR"/venv/Scripts/activate
+python -m pip install cmake==3.26.3
 python -m pip install -r "$WORKING_DIR"/requirements.txt
 deactivate
